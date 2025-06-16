de12afae25eca8286f69dddab0d30cec3fc6f275
Sokkhann <sokkhann.pol02@gmail.com>
2025-06-16 10:31:38 +0700

    add sendNotfication

---
+++ vars/sendNotification.groovy
@@ -0,0 +1,12 @@
+def call(Map params = [:]) {
+    def message = params.message ?: 'Build completed'
+    def recipient = params.recipient ?: 'sokkhann.pol02@gmail.com'
+
+    emailext (
+        subject: "Jenkins Notification: ${env.JOB_NAME}",
+        body: "${message}\n\nCheck build: ${env.BUILD_URL}",
+        recipientProviders: [],
+        to: recipient,
+        mimeType: 'text/plain'
+    )
+}
