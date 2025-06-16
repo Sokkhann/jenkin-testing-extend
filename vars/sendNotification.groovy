8008a6691debef046494e3265c9703896955c25a
Sokkhann <sokkhann.pol02@gmail.com>
2025-06-16 11:53:34 +0700

    update

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,12 +1,19 @@
 def call(Map params = [:]) {
     def message = params.message ?: 'Build completed'
-    def recipient = params.recipient ?: 'sokkhann.pol02@gmail.com'
+    def chatId = params.chatId ?: '860211301'
+    def botToken = params.botToken ?: '8060632961:AAGtD4G8MxzZnUDW9xfEAsi251gdwTtB-jw'
 
-    emailext (
-        subject: "Jenkins Notification: ${env.JOB_NAME}",
-        body: "${message}\n\nCheck build: ${env.BUILD_URL}",
-        recipientProviders: [],
-        to: recipient,
-        mimeType: 'text/plain'
-    )
+    def fullMessage = """
+<b>${message}</b>
+Job: ${env.JOB_NAME}
+Build: ${env.BUILD_NUMBER}
+URL: ${env.BUILD_URL}
+"""
+
+    sh """
+    curl -s -X POST https://api.telegram.org/bot${botToken}/sendMessage  \\
+    -d chat_id=${chatId} \\
+    -d text="${fullMessage}" \\
+    -d parse_mode="HTML"
+    """
 }
